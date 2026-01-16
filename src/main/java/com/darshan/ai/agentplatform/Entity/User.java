39e323cf263bb5da9b6d20aefbc2011f671a6e49
Darshan <darshanshrinandi@gmail.com>
2026-01-16 14:54:49 +0530

    Fix User entity id mapping to match database schema

--- src/main/java/com/darshan/ai/agentplatform/Entity/User.java
+++ src/main/java/com/darshan/ai/agentplatform/Entity/User.java
@@ -20,6 +20,7 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false)
