c58ebe15f982c6da3682d0b0b7b54a439ab85db4
Darshan <darshanshrinandi@gmail.com>
2026-01-16 14:43:52 +0530

    fixed UserService

--- src/main/java/com/darshan/ai/agentplatform/Service/UserService.java
+++ src/main/java/com/darshan/ai/agentplatform/Service/UserService.java
@@ -25,7 +25,7 @@ public class UserService {
             log.error("User already exists");
         }
 
-        passwordEncoder.encode(user.getPassword());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
     }
