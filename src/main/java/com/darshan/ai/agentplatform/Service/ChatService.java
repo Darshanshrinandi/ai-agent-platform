d44ee7e77de0795812f021dbbda88e8ee50758e8
Darshan <darshanshrinandi@gmail.com>
2026-01-16 17:50:17 +0530

    Fixed in ChatSerVice

--- src/main/java/com/darshan/ai/agentplatform/Service/ChatService.java
+++ src/main/java/com/darshan/ai/agentplatform/Service/ChatService.java
@@ -2,6 +2,7 @@ package com.darshan.ai.agentplatform.Service;
 
 import com.darshan.ai.agentplatform.Entity.Project;
 import com.darshan.ai.agentplatform.Entity.Prompt;
+import com.darshan.ai.agentplatform.Repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,11 +21,14 @@ public class ChatService {
     private ProjectService projectService;
 
     @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
     private ChatHistoryService chatHistoryService;
 
     public String chatWithProjects(Long projectId, String userMessage) {
 
-        Project project = projectService.getProjectById(projectId);
+        Project project = projectRepository.findById(projectId).orElseThrow(() -> new RuntimeException("Project Not Found"));
 
         List<Prompt> prompts = promptService.getPromptsByProject(projectId);
 
